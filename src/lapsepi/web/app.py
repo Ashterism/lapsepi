b8db68411cba9dd2df8ff6db180ab0e656b18f41
Ash <33597520+Ashterism@users.noreply.github.com>
2026-04-05 21:14:00 +0400

    wireframed webapp done

---
+++ src/lapsepi/web/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+# from skypi.services.daily_report import get_daily_report
+
+"""
+python -m lapsepi.web.app
+"""
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # report = get_daily_report()
+
+    return render_template(
+        "index.html"
+        # , 
+        # go_no_go = report["go_no_go"],
+        # at_a_glance = report["at_a_glance"],
+        # summary_moon = report["summary_moon"],
+        # next_good_night = report["next_good_night"],
+        # astro_session_data = report["astro_session_data"],
+    )
+
+@app.route("/styleguide")
+def styleguide():
+    return render_template(
+        "styleguide.html"
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
