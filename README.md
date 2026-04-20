91588a80d17de33a5c4a40597216155823d3dfa8
Ash <33597520+Ashterism@users.noreply.github.com>
2026-04-20 20:35:35 +0400

    video download added

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 
 
 requires ffmeg
+
+
+at install...
+
+open lapsepi.service
+>
+edit the path to reflect where you installed the app
+
+i.e. update username/path before copying to /etc/systemd/system/
+
+then...run it...
+
+then...
+
+sudo systemctl daemon-reload
+sudo systemctl enable monipi.service
+sudo systemctl start monipi.servic
--- src/lapsepi/__main__.py
+++ src/lapsepi/__main__.py
@@ -13,14 +13,9 @@ def main():
 
     # start webserver (while... until told to stop?)
 
-    interval = 10
-    runtime = 20
-    photos = int(runtime / interval)
-
-    for i in range(photos):
-        camera.take_image()
-        if i < photos - 1:
-            time.sleep(interval)
+    # if system tries to shut you down... do it gracefully and log it
+    # guessing it's just going to be the power being pulled as no button..
+    # to which, i guess thee is no real defence.
 
 
 if __name__ == "__main__":
